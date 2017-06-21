78bca015bc3893311866dd2d6020228225c1f479
k.c. schroeter <kesc0188@colorado.edu>
2017-06-21 14:12:37 -0400

    Get images on page

--- deepstache.js
+++ deepstache.js
@@ -1,6 +1,7 @@
 // Capture all images on the page
+var pageImages = document.getElementsByTagName("img");
 
-// Check if each image has a moustache
+// Check if each image has a human face
     //Add atribute to the file if it does
     //Create smaller list with this subset
     
