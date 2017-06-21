86a672b968f58e5c15b1d72e9b5760a96d8b6632
k.c. schroeter <kesc0188@colorado.edu>
2017-06-21 14:06:05 -0400

    Outline very broad sudo code

---
+++ deepstache.js
@@ -0,0 +1,9 @@
+// Capture all images on the page
+
+// Check if each image has a moustache
+    //Add atribute to the file if it does
+    //Create smaller list with this subset
+    
+ // Add Moustache to Each Picture in the List
+ 
+ // Replace original pictures with edited pictures
